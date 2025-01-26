ff9f4c3506c373e03d2116b7b3b248df586eef35
Murali <muraliparvatham.p@gmail.com>
2025-01-26 22:57:15 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./index.css";
+import Profile from "./Profile.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+}
